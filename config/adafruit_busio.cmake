0357c42dede231e379a6af1b8f46b8a5834cb6ee
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:23:41 +0900

    update ubinos build scripts

--- config/adafruit_busio.cmake
+++ config/adafruit_busio.cmake
@@ -5,6 +5,7 @@
 #
 
 set(INCLUDE__ADAFRUIT_BUSIO TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} Adafruit_BusIO_wrapper)
 
 set_cache_default(ADAFRUIT_BUSIO__BASE_DIR "${PROJECT_LIBRARY_DIR}/Adafruit_BusIO" STRING "Adafruit BusIO project base dir")
 
